60adb72dcf9933f00ed117f78420b460e94cb2bd
Mark Matheisl <matheislmarek@gmail.com>
2019-09-11 18:49:13 +0200

    .

---
+++ m.py
@@ -0,0 +1,39 @@
+import math
+from pylab import *
+from numpy import NaN
+
+def mandelbrot(z , c , n=40):
+    if abs(z) > 1000:
+        return float("nan")
+    elif n > 0:
+        return mandelbrot(z ** 2 + c, c, n - 1)
+    else:
+        return z ** 2 + c
+
+print("\n".join(["".join(["#" if not math.isnan(mandelbrot(0, x + 1j * y).real) else " "
+                 for x in [a * 0.02 for a in range(-80, 30)]])
+                 for y in [a * 0.05 for a in range(-20, 20)]])
+     )
+
+def m(a):
+	z = 0
+	for n in range(1, 100):
+		z = z**2 + a
+		if abs(z) > 2:
+			return n
+	return NaN
+ 
+X = arange(-2, .5, .002)
+Y = arange(-1,  1, .002)
+Z = zeros((len(Y), len(X)))
+ 
+for iy, y in enumerate(Y):
+	print (iy, "of", len(Y))
+	for ix, x in enumerate(X):
+		Z[iy,ix] = m(x + 1j * y)
+ 
+imshow(Z, cmap = plt.cm.prism, interpolation = 'none', extent = (X.min(), X.max(), Y.min(), Y.max()))
+xlabel("Re(c)")
+ylabel("Im(c)")
+savefig("mandelbrot_python.png")
+show()
