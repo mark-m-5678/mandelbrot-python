39522772a3312313acac827e7352a86d9bb43fa1
Mark Matheisl <matheislmarek@gmail.com>
2019-09-16 10:35:25 +0200

    Changes of some stuff I guess

--- mandelbrot.py
+++ mandelbrot.py
@@ -22,16 +22,16 @@ def m(a):
 		if abs(z) > 2:
 			return n
 	return NaN
- 
+
 X = arange(-2, .5, .002)
 Y = arange(-1,  1, .002)
 Z = zeros((len(Y), len(X)))
- 
+
 for iy, y in enumerate(Y):
 	print (iy, "of", len(Y))
 	for ix, x in enumerate(X):
 		Z[iy,ix] = m(x + 1j * y)
- 
+
 imshow(Z, cmap = plt.cm.prism, interpolation = 'none', extent = (X.min(), X.max(), Y.min(), Y.max()))
 xlabel("Re(c)")
 ylabel("Im(c)")
