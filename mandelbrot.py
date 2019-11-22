c9622242bf8e7833d93f87da0bfd83d204026aa7
Mark Matheisl <matheislmarek@gmail.com>
2019-11-22 10:33:06 +0100

    Some changes or something

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,7 +2,8 @@ import math
 from pylab import *
 from numpy import NaN
 
-def mandelbrot(z , c , n=40):
+
+def mandelbrot(z, c, n=40):
     if abs(z) > 1000:
         return float("nan")
     elif n > 0:
@@ -10,30 +11,33 @@ def mandelbrot(z , c , n=40):
     else:
         return z ** 2 + c
 
+
 print("\n".join(["".join(["#" if not math.isnan(mandelbrot(0, x + 1j * y).real) else " "
-                 for x in [a * 0.02 for a in range(-80, 30)]])
+                          for x in [a * 0.02 for a in range(-80, 30)]])
                  for y in [a * 0.05 for a in range(-20, 20)]])
-     )
+      )
+
 
 def m(a):
-	z = 0
-	for n in range(1, 100):
-		z = z**2 + a
-		if abs(z) > 2:
-			return n
-	return NaN
+    z = 0
+    for n in range(1, 100):
+        z = z ** 2 + a
+        if abs(z) > 2:
+            return n
+    return NaN
+
 
 X = arange(-2, .5, .002)
-Y = arange(-1,  1, .002)
+Y = arange(-1, 1, .002)
 Z = zeros((len(Y), len(X)))
 
 for iy, y in enumerate(Y):
-	print (iy, "of", len(Y))
-	for ix, x in enumerate(X):
-		Z[iy,ix] = m(x + 1j * y)
+    print(iy, "of", len(Y))
+    for ix, x in enumerate(X):
+        Z[iy, ix] = m(x + 1j * y)
 
-imshow(Z, cmap = plt.cm.prism, interpolation = 'none', extent = (X.min(), X.max(), Y.min(), Y.max()))
+imshow(Z, cmap=plt.cm.prism, interpolation='none', extent=(X.min(), X.max(), Y.min(), Y.max()))
 xlabel("Re(c)")
 ylabel("Im(c)")
-savefig("mandelbrot_python.svg")
+# savefig("mandelbrot_python.svg")
 show()
